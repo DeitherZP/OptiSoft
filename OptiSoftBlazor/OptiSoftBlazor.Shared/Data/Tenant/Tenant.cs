03e36b583dcf8a9501a06c60ff11cc1f9184f93b
DeitherZP <deiterzamora@gmail.com>
2026-02-03 06:58:30 -0500

    Botones para el llamado a la API para generar los reportes

---
+++ OptiSoftBlazor/OptiSoftBlazor.Shared/Data/Tenant/Tenant.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace OptiSoftBlazor.Shared.Data.Tenant
+{
+    public class Tenant
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public string? ID { get; set; }
+
+        public string? Name { get; set; }
+
+        public string? ConnectionString { get; set; }
+    }
+}
